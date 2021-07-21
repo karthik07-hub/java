3bb524e43f50231cc1f1068d77ad732cdf6abd3d
karthik07-hub <87748375+karthik07-hub@users.noreply.github.com>
2021-07-21 21:59:43 +0530

    Add files via upload

---
+++ StudentClass/src/StudentClass.java
@@ -0,0 +1,9 @@
+public class StudentClass{  
+ int id;
+ String name;  
+ public static void main(String args[]){  
+  StudentClass s1=new StudentClass(); 
+  System.out.println(s1.id);  
+  System.out.println(s1.name);  
+ }  
+}  
